d6db610f1b77430fc431b0dc777bc26f253ac5fc
difuoan <lucas.j.venturini@gmail.com>
2024-05-23 14:00:39 +0200

    making it a component library

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,19 @@ export default defineConfig({
     copyPublicDir: false,
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
-      formats: ['es']
-    }
+      formats: ['es'],
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
   }
 })
