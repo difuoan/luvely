25fd27cb3e3cc2a82b8bf9e727cf3e7d40c133d8
difuoan <lucas.j.venturini@gmail.com>
2024-05-28 07:18:01 +0200

    added console logs to the actions tab in the storybook ui

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,12 @@
 import type { Preview } from '@storybook/vue3'
+import '@storybook/addon-console';
+
+import { setConsoleOptions } from '@storybook/addon-console';
+
+const panelExclude = setConsoleOptions({}).panelExclude;
+setConsoleOptions({
+  panelExclude: [...panelExclude, /deprecated/],
+});
 
 const preview: Preview = {
   parameters: {
